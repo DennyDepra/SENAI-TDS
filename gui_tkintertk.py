ee612705458e3f2a5ad12d5a6de8087341fd9926
Denny.depra345@gmail.com <arthurmuraroto@gmail.com>
2026-07-02 21:15:59 -0300

    janelaPrincipal

--- gui_tkintertk.py
+++ gui_tkintertk.py
@@ -1,1 +1,16 @@
+import tkinter as tk
 
+from tkinter import Tk
+
+janela = tk.Tk()
+janela.title("meu app")
+janela.geometry("400x300")
+
+#loop eventos
+label = tk.Label(janela, text="Olá, mundo!", font=("Arial", 20))
+label.pack(pady=20, padx=20)
+
+texto = tk.Text(janela,width=20)
+texto.pack(pady=20)
+
+janela.mainloop()
